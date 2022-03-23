151b4ca3c72ed3f6f3e15b7ed3e88de00cebb584
charleytran2001 <44280830+charleytran2001@users.noreply.github.com>
2022-03-22 17:07:06 -0700

    aaaaaaaaaaaaaaaaaaaaaaa
    why do i need async to get file length

--- js/time.js
+++ js/time.js
@@ -7,33 +7,38 @@ let endTime;
 
 function retrieveMediaTime() {
     // Retreives total length of media
-    media.addEventListener('loadedmetadata', function() {
+    media.addEventListener('durationchange', function() {
         mediaDuration = this.duration;
-        console.log("Length: " + mediaDuration);
+        // console.log("Length: " + mediaDuration);
         
-        // If playback is set to full song, then it plays until the end
-        if(playbackFull) {
-            if(randomStartTime) {
-                startTime = Math.floor(Math.random() * mediaDuration);
-                media.currentTime = startTime;
-            } else {
-                // Starts media at specified start time
-                media.currentTime = startTime;
-            }
-            endTime = mediaDuration;
-            // console.log("Full: " + endTime);
+        // // If playback is set to full song, then it plays until the end
+        // if(playbackFull) {
+        //     if(randomStartTime) {
+        //         startTime = Math.floor(Math.random() * mediaDuration);
+        //         media.currentTime = startTime;
+        //     } else {
+        //         // Starts media at specified start time
+        //         media.currentTime = startTime;
+        //     }
+        //     endTime = mediaDuration;
+        //     // console.log("Full: " + endTime);
 
-        // Otherwise it makes sure that the playback length doesn't go past song length
-        } else {
-            if(randomStartTime) {
-                startTime = Math.floor(Math.random() * (mediaDuration - playbackLength));
-                media.currentTime = startTime;
-            } else {
-                // Starts media at specified start time
-                media.currentTime = startTime;
-            }
-            endTime = startTime + playbackLength;
-            // console.log("Partial: " + endTime);
-        }
+        // // Otherwise it makes sure that the playback length doesn't go past song length
+        // } else {
+        //     if(randomStartTime) {
+        //         startTime = Math.floor(Math.random() * (mediaDuration - playbackLength));
+        //         media.currentTime = startTime;
+        //     } else {
+        //         // Starts media at specified start time
+        //         media.currentTime = startTime;
+        //     }
+        //     endTime = startTime + playbackLength;
+        //     // console.log("Partial: " + endTime);
+        // }
+        console.log(mediaDuration);
     });
+
+    
+
+    console.log(mediaDuration);
 }
